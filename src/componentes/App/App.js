fc1d81e7d8227956af7439bbf1c742e6a7da2f12
Masih-Saldanha <masih.saldanha@gmail.com>
2022-03-24 13:15:05 -0300

    Header pronto e estrutura base de BrowseRouter pronta

--- src/componentes/App/App.js
+++ src/componentes/App/App.js
@@ -1,10 +1,33 @@
-import { useState } from "react/cjs/react.production.min"
+import { useState, useEffect } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import axios from "axios"
+
+import Header from "../Header/Header"
+
 import "../App/reset.css"
 import "../App/styles.css"
+import MovieSelection from "../MovieSelection/MovierSelection"
 
 export default function App() {
+    console.log("Renderizei App");
+    const [movieList, setMovieList] = useState([])
+    console.log("Lista de Filmes: ", movieList);
+    useEffect(() => {
+        console.log("Entrei no useEffect");
+        const promise = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+        promise.then(response => {
+            const { data } = response;
+            console.log("Dados da API:", data);
+            setMovieList(data);
+        })
+        console.log("Sai do useEffect");
+    }, [])
     return (
-        <h1>TESTE</h1>
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<MovieSelection />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
---
+++ src/componentes/MovieSelection/MovierSelection.js
@@ -0,0 +1,5 @@
+export default function MovieSelection() {
+    return (
+        <h1>MovieSelection</h1>
+    )
+}
