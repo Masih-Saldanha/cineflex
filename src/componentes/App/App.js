4b94f036d62679409b81807d1170e343079a6ed0
Masih-Saldanha <masih.saldanha@gmail.com>
2022-03-24 11:45:30 -0300

    Base pronta com Reset e Style Geral

---
+++ src/componentes/App/App.js
@@ -0,0 +1,10 @@
+import { useState } from "react/cjs/react.production.min"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "../App/reset.css"
+import "../App/styles.css"
+
+export default function App() {
+    return (
+        <h1>TESTE</h1>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import reactDom from "react-dom";
+import App from "./componentes/App/App";
+
+const root = document.querySelector(".root");
+reactDom.render(<App />, root);
